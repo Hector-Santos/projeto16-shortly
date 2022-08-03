1905fd1188ae613c4caef2849b5dbe6fd00df35a
Hector-Santos <habs.404@gmail.com>
2022-08-03 15:50:32 -0300

    teste da conexão heroku

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import express from 'express';
-import router from './routes/router.js';
-import cors from 'cors';
-import dotenv from 'dotenv';
-dotenv.config();
+const http = require('http');
+const port = process.env.PORT || 3000
 
-
-const server = express();
-server.use(cors())
-server.use(express.json());
-server.use(router);
-
-
-
-server.listen(process.env.PORT, () => {
-  console.log('Server is listening on port 4000.');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
 });
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+})
