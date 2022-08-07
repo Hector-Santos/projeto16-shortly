ff4bd4671d6f40af7d442c96254c9921a83d9dca
Hector-Santos <habs.404@gmail.com>
2022-08-07 14:46:33 -0300

    criar getUrlById

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -20,16 +20,16 @@ export async function postUrl(req, res) {
 }
 
 export async function getUrlById(req, res) {
-    const id =  req.params.id
-    console.log(email)
-    const shortUrl = nanoid(5)
+    const id = req.params.id
     try{
-      const {rows:user} = await connection.query('SELECT * FROM users WHERE email=$1',[email])
-      if(!user.length) return res.sendStatus(409)
-      
-      await connection.query('INSERT INTO urls (url,"shortUrl","userId") VALUES ($1,$2,$3)', [url, shortUrl, user[0].id ])
-        res.status(201).send({shortUrl: shortUrl});
-      }catch(error){
+      const {rows:url} = await connection.query('SELECT * FROM urls WHERE id=$1',[id])
+      if(!url.length) return res.sendStatus(404)
+        res.status(200).send({
+            id: id,
+	        shortUrl: url[0].shortUrl,
+	        url: url[0].url
+      })
+    }catch(error){
         res.sendStatus(400)
       }
   
--- src/routes/urlRouter.js
+++ src/routes/urlRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { postUrl } from '../controllers/urlController.js';
+import { getUrlById, postUrl } from '../controllers/urlController.js';
 import { validate } from '../middlewares/validationMiddleware.js';
 import urlSchema from '../schemas/urlSchema.js';
 import { authentication } from '../middlewares/authMiddleware.js';
 
 const urlRouter = express.Router();
 urlRouter.post("/urls/shorten", (req, res, next) => validate(req, res, next, urlSchema), authentication, postUrl);
+urlRouter.get("/urls/:id", getUrlById);
 
 
 
