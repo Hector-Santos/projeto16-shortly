53f38512e5300aa93ae09459097ec3785c798d7c
Hector-Santos <habs.404@gmail.com>
2022-08-08 18:22:09 -0300

    implentar encriptação de senha

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,14 @@
 import  jwt  from 'jsonwebtoken';
 import  {userRepository}  from '../repositories/usersRepository.js';
+import bcrypt from 'bcrypt';
 
 export async function signUp(req, res) {
     const user = req.body
     try{
       const {rows:repetido} = await userRepository.getUser(user.email)
       if(repetido.length) return res.sendStatus(409)
-    await userRepository.insertUser(user.name, user.email, user.password)
+      const encrypted = bcrypt.hashSync(user.password, 10)
+    await userRepository.insertUser(user.name, user.email, encrypted)
       res.sendStatus(201);
     }catch(error){
       res.sendStatus(400)
@@ -17,7 +19,8 @@ export async function signIn(req, res) {
   const user = req.body
   try{
     const {rows:login} = await userRepository.getUser(user.email)
-    if(!login.length || user.password !== login[0].password) return res.sendStatus(401)
+
+    if(!login.length || !bcrypt.compareSync(user.password, login[0].password) ) return res.sendStatus(401)
     const dados = { email: user.email };
     const chaveSecreta = process.env.JWT_SECRET;
     const token = jwt.sign(dados, chaveSecreta)
